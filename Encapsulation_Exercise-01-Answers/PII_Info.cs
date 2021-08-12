308d915abec23fe040c17209f448907e08915823
johndward01 <johndward01@gmail.com>
2021-08-11 23:21:26 -0500

    answer init

---
+++ Encapsulation_Exercise-01-Answers/PII_Info.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulation_Exercise_01_Answers
+{
+    class PII_Info
+    {
+        // BONUS
+        // TODO: Create 1 private readonly field _secret of type string
+        private readonly string _secret;
+
+
+
+        // TODO: Create 4 private fields
+        private int _userID;
+        private string _privateKey;
+        private decimal _walletBalance;
+        private bool _isAuthenticated;
+
+
+
+        // BONUS
+        // TODO: Assign a value to the field
+        public PII_Info(string secret)
+        {
+            _secret = secret;
+        }
+
+
+
+        // TODO: Create 2 private auto-implemented properties
+        private string UserName { get; set; }
+        private string Password { get; set; }
+
+
+
+        #region// TODO: Create 4 public methods to get (read) those fields
+        public int GetUserID()
+        {
+            return _userID;
+        }
+
+        public string GetEncryptedString()
+        {
+            return _privateKey;
+        }
+
+        public decimal GetBalance()
+        {
+            return _walletBalance;
+        }
+
+        public bool GetAuthenticatedStatus()
+        {
+            return _isAuthenticated;
+        }
+
+        #endregion
+
+
+
+        #region// TODO: Create 4 public methods to set (write) those fields
+        public void SetUserID(int userID)
+        {
+            _userID = userID;
+        }
+
+        public void SetEncryptedString(string encryptedString)
+        {
+            _privateKey = encryptedString;
+        }
+
+        public void SetBalance(decimal balance)
+        {
+            _walletBalance = balance;
+        }
+
+        public void SetAuthenticatedStatus(bool authenticatedStatus)
+        {
+            _isAuthenticated = authenticatedStatus;
+        }
+
+        #endregion
+
+
+
+        // TODO: Create 1 method called SetInfo() which sets the private properties
+        public void SetInfo(string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
+        }
+
+
+
+
+        // TODO: Create 1 method called InformationReport() which creatively displays all the values 
+        public void InformationReport()
+        {
+            Console.Write($"Username: {UserName}\n" +
+                          $"Password: {Password}\n" +
+                          $"User ID: {_userID}\n" +
+                          $"Private Key: {_privateKey}\n" +
+                          $"Wallet Balance: {string.Format(_walletBalance.ToString())}\n" +
+                          $"Is Authenticated: {_isAuthenticated}\n\n" +
+                          $"SECRET: {_secret}");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
--- Encapsulation_Exercise-01-Answers/Program.cs
+++ Encapsulation_Exercise-01-Answers/Program.cs
@@ -4,7 +4,26 @@ namespace Encapsulation_Exercise_01_Answers
 {
     class Program
     {
-        // TODO: Create a FULL PROPERTY by using the code snippet: type propfull and then press tab twice (propfull + tab + tab)
+
+        #region// TODO: Create a Personally Identifiable Information class (Name is PII_Info)
+        // TODO: Create 4 private fields
+        // TODO: Create 4 public methods to get (read) those fields
+        // TODO: Create 4 public methods to set (write) those fields
+
+        // TODO: Create 2 private auto-implemented properties 
+        // TODO: Creaet 1 method called SetInfo() which sets the private properties
+        // TODO: Create 1 method called InformationReport() which creatively displays the 2 private properties 
+
+        // BONUS
+        // TODO: Create 1 private readonly field called _secret of type string
+        // Thought Experiment: How can you initialize this field with a value?
+        // Answer Here: In a field declaration, readonly indicates that assignment to the field can only occur as part of the declaration or in a constructor in the same class. 
+        // TODO: Assign a value to the _secret field
+
+        #endregion
+
+
+        #region// TODO: Create a FULL PROPERTY by using the code snippet: type propfull and then press tab twice (propfull + tab + tab)
         private int myVar;
 
         public int MyProperty
@@ -13,12 +32,13 @@ namespace Encapsulation_Exercise_01_Answers
             set { myVar = value; }
         }
 
+        #endregion
+
         static void Main(string[] args)
         {
             // Thought Experiment: Will the following code work or cause a compiler error?
-            // Console.WriteLine(myVar);
-            // TODO: It will cause an error because the myVar field has been encapsulated by the private access modifier
-
+            // Console.WriteLine(myVar); 
+            // Answer Here: It will cause an error because the myVar field has been encapsulated by the private access modifier
 
 
 
@@ -28,7 +48,22 @@ namespace Encapsulation_Exercise_01_Answers
 
 
 
+            // TODO: Create an instance of the PII_Info class
+            var pii = new PII_Info("!@@fghfghh$#@#LKJ&dh#$lkjd$KLJsfd");
 
+
+
+            // TODO: Set the values for the PII_Info instance
+            pii.SetAuthenticatedStatus(true);
+            pii.SetBalance(1000.00M);
+            pii.SetEncryptedString("alsde#@%##@!!!!LESDKHJF*##@$UHEslkdjhfjkh31f3298hkjqh");
+            pii.SetUserID(100338);
+            pii.SetInfo("johndoe4321", "JohnDoe1234!!!");
+
+
+
+            // TODO: Call the InformationReport() method 
+            pii.InformationReport();
 
         }
     }
