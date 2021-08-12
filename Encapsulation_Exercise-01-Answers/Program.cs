36a638c9fbc4437720e68a06c9fc02db9083057a
johndward01 <johndward01@gmail.com>
2021-08-11 22:05:00 -0500

    Add project files.

---
+++ Encapsulation_Exercise-01-Answers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Encapsulation_Exercise_01_Answers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
