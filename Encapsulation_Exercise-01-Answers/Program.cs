30a1bc17a97fb471bdffe54a45f1432853ad3a86
johndward01 <johndward01@gmail.com>
2021-08-11 22:21:27 -0500

    update

--- Encapsulation_Exercise-01-Answers/Program.cs
+++ Encapsulation_Exercise-01-Answers/Program.cs
@@ -4,9 +4,32 @@ namespace Encapsulation_Exercise_01_Answers
 {
     class Program
     {
+        // TODO: Create a FULL PROPERTY by using the code snippet: type propfull and then press tab twice (propfull + tab + tab)
+        private int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Thought Experiment: Will the following code work or cause a compiler error?
+            // Console.WriteLine(myVar);
+            // TODO: It will cause an error because the myVar field has been encapsulated by the private access modifier
+
+
+
+
+            // Thought Experiment: How would you access the myVar variable and print it out to the console?
+            // TODO: Console.WriteLine(MyProperty); 
+            // Why: ? Because the property is public, it is not encapsulated, therefore we can access it directly. And when we call it's getter method, we return myVar
+
+
+
+
+
         }
     }
 }
